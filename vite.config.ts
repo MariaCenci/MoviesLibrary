bff360459dd45c7bc68a66265063f4d01a192f6a
Maria Fernanda Cenci <mariafcenci@gmail.com>
2023-11-21 20:35:32 +0100

    test github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-build: {
-   outDir: 'dist',
-   target: 'esnext',
-  
- },
+   base: "/MoviesLibrary",
 
-    base: 'https://movieslibrary-keoi.onrender.com/',
   
 })
+/*build: {
+   outDir: 'dist',
+   target: 'esnext',
+ 
+ },*/
